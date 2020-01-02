5f379588a82f0f34616fbbfe0d88a420cbbf9ec9
Justin Miller <jayman1294@gmail.com>
2020-01-02 11:51:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,8 +30,19 @@ class Student
     LIMIT 1 
     SQL
     
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def self.all_students_in_grade_9
     
   end
+  
+  
+  
+  
+  
   
   def save
     sql = <<-SQL
