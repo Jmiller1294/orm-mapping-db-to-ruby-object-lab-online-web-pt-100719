670c1e0ae5d9c0bef608748f5460392ff08aff58
Justin Miller <jayman1294@gmail.com>
2020-01-02 12:29:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,11 +57,11 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(number)
-    x = number
+   
     
     sql = <<-SQL
     SELECT*FROM students
-    WHERE grade = x
+    WHERE grade = number
     SQL
     
     DB[:conn].execute(sql)
