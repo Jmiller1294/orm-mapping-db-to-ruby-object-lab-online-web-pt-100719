f5deec8ba689e4ca292cbe4bfc6804c48dfdd758
Justin Miller <jayman1294@gmail.com>
2020-01-02 11:56:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,8 +36,24 @@ class Student
   end
   
   def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT FROM students
+    WHERE grade = 9 
+    SQL
     
   end
+  
+  def self.students_below_12th_grade
+  end
+  
+  def self.first_X_students_in_grade_10 
+  end 
+  
+  def self.first_student_in_grade_10 
+  end 
+  
+  def self.all_students_in_grade_X
+  end 
   
   
   
