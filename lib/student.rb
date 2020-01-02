f6475eea92f56e0853e1fe939552d1caebbd479b
Justin Miller <jayman1294@gmail.com>
2020-01-02 12:48:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,6 +56,7 @@ class Student
     # DB[:conn].execute(sql)
     sql = "SELECT * FROM students WHERE grade < 12"
     DB[:conn].execute(sql)
+    
   end
   
   def self.first_X_students_in_grade_10(x)
