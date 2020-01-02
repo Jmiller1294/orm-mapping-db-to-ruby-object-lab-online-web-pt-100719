ae490865f5c2755ca298ca55e8c36778a90fde1e
Justin Miller <jayman1294@gmail.com>
2020-01-02 12:48:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,9 @@ class Student
   end 
   
   def self.first_student_in_grade_10 
-    
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    first_student_row = DB[:conn].execute(sql)[0]
+    self.new_from_db(first_student_row)
   end 
   
   def self.all_students_in_grade_X
